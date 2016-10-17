d432ff958f67329401632648abb9c9f9eb3433a6
xiaochaihu <xiaochaihu>
2016-10-17 22:28:32 +0800

    learn about split HBase task

--- hdata-hbase/src/main/java/com/github/stuxuhai/hdata/plugin/reader/hbase/HBaseSplitter.java
+++ hdata-hbase/src/main/java/com/github/stuxuhai/hdata/plugin/reader/hbase/HBaseSplitter.java
@@ -80,6 +80,8 @@ public class HBaseSplitter extends Splitter {
 				/**
 				 * Gets the starting and ending row keys for every region in the currently open table.
 				 * This is mainly useful for the MapReduce integration.
+				 * return : Pair<[startKey1, startKey2, startKey3],
+				 * 				 [endKey1  , endKey2  , endKey3  ]>
 				 */
 				Pair<byte[][], byte[][]> startEndKeysPair = regionLocator.getStartEndKeys();
 				table.close();
@@ -97,44 +99,73 @@ public class HBaseSplitter extends Splitter {
 				 */
 				byte[][] endKeys = startEndKeysPair.getSecond();
 
+				/**
+				 * startKeys.length == 1 说明只有一个 region
+				 */
 				if (startKeys.length == 1) {
 					Pair<byte[], byte[]> pair = new Pair<>();
 					pair.setFirst(startRowkeyBytes);
 					pair.setSecond(endRowkeyBytes);
 					selectedPairList.add(pair);
 				} else {
+					/**
+					 * startRowkeyBytes.length 和 endRowkeyBytes.length 同时等于 0， 说明应该抽取所有数据
+					 */
 					if (startRowkeyBytes.length == 0 && endRowkeyBytes.length == 0) {
+						/**
+						 * 分别把每个 region 的 startKey 及 endKey 作为分解任务的界限
+						 * 任务数等于 region 的总数
+						 */
 						for (int i = 0, len = startKeys.length; i < len; i++) {
 							Pair<byte[], byte[]> pair = new Pair<>();
 							pair.setFirst(startKeys[i]);
 							pair.setSecond(endKeys[i]);
 							selectedPairList.add(pair);
 						}
-					} else if (endRowkeyBytes.length == 0) {
+					}
+					/**
+					 * startRowkeyBytes.length != 0， 而 endRowkeyBytes.length == 0,说明应该从 startRowkey 开始抽数，一直到抽完所有数据
+					 */
+					else if (endRowkeyBytes.length == 0) {
 						for (int i = 0, len = startKeys.length; i < len; i++) {
+							/**
+							 * Bytes.compareTo 方法返回值解释： 0 if equal, < 0 if left is less than right, etc.
+                             * 如果当前 region 的 endKeys 大于抽数任务的 startRowkey，说明该 region 至少有部分（也可能是整个 region）数据需要被抽取
+							 */
 							if (Bytes.compareTo(endKeys[i], startRowkeyBytes) >= 0) {
 								Pair<byte[], byte[]> pair = new Pair<>();
-								pair.setFirst(Bytes.compareTo(startKeys[i], startRowkeyBytes) >= 0 ? startKeys[i]
-										: startRowkeyBytes);
+								/**
+                                 * 如果当前 startKey 大于抽数任务的 startRowkey，说明该 region 应该从 startKey 开始抽数，一直到 endKey，
+								 * 否则该 region 应该从 startRowkey 开始抽数，一直到 endKey
+								 */
+								pair.setFirst(Bytes.compareTo(startKeys[i], startRowkeyBytes) >= 0 ? startKeys[i] : startRowkeyBytes);
 								pair.setSecond(endKeys[i]);
 								selectedPairList.add(pair);
 							}
 						}
-					} else {
+					}
+					/**
+					 * startRowkeyBytes.length 和 endRowkeyBytes.length 都不等于 0， 说明应该从 startRowkey 开始抽数，一直到 endRowkey
+					 */
+					else {
 						for (int i = 0, len = startKeys.length; i < len; i++) {
+							/**
+                             * len == 1，说明只有一个 region，因此直接设置 startRowkey 和 endRowkey 即可
+							 */
 							if (len == 1) {
 								Pair<byte[], byte[]> pair = new Pair<>();
 								pair.setFirst(startRowkeyBytes);
 								pair.setSecond(endRowkeyBytes);
 								selectedPairList.add(pair);
 								break;
-							} else if (Bytes.compareTo(endKeys[i], startRowkeyBytes) >= 0
-									&& Bytes.compareTo(endRowkeyBytes, startKeys[i]) >= 0) {
+							}
+							/**
+                             * start <= startRowkey < endRowkey <= endKey
+							 */
+							else if (Bytes.compareTo(endKeys[i], startRowkeyBytes) >= 0 && Bytes.compareTo(endRowkeyBytes, startKeys[i]) >= 0) {
 								Pair<byte[], byte[]> pair = new Pair<>();
-								pair.setFirst(Bytes.compareTo(startKeys[i], startRowkeyBytes) >= 0 ? startKeys[i]
-										: startRowkeyBytes);
-								pair.setSecond(
-										Bytes.compareTo(endKeys[i], endRowkeyBytes) <= 0 ? endKeys[i] : endRowkeyBytes);
+								pair.setFirst(Bytes.compareTo(startKeys[i], startRowkeyBytes) >= 0 ? startKeys[i] : startRowkeyBytes);
+								pair.setSecond(Bytes.compareTo(endKeys[i], endRowkeyBytes) <= 0 ? endKeys[i] : endRowkeyBytes);
 								selectedPairList.add(pair);
 							}
 						}
